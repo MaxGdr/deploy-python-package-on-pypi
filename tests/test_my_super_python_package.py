5aa9303948dd299c4c2124d794d15ae9a0ee9943
Maxime GENDRE <gendre.maxime@gmail.com>
2021-01-18 10:10:50 +0100

    Init python package structure

---
+++ tests/test_my_super_python_package.py
@@ -0,0 +1,10 @@
+import pytest
+from my_super_python_package.skeleton import fib
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
